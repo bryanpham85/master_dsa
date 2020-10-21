c1148ba6f0607872634551c007b1fb1f0159e745
Bryan <duchungpham12d2@gmail.com>
2020-10-21 16:16:05 +0700

    update methods

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -15,9 +15,9 @@ void LinkedList::Create(){
 
 void LinkedList::Traverse(){
     //just simply traverse all item then print the data out
-    Node *current = this->head->next;
+    Node *current = this->head;
     int index = 0;
-    while (current != NULL && index < this->nCount)
+    while (current != NULL)
     {
         cout << "The data of Node " << index + 1 << " is " << current->data << "\n";
         index++;
@@ -38,4 +38,50 @@ void LinkedList::Clear(){
     }
 
     this->nCount = 0;
+}
+
+int LinkedList::Size(){
+    return this->nCount;
+}
+
+void LinkedList::InsertHead(int data){
+    Node *newNode = new Node();
+    newNode->data = data;
+    Node *previousHead = this->head;
+    this->head = newNode;
+    newNode->next = previousHead;
+    cout << "the new node add at head. List size is: " << ++this->nCount <<"\n";
+}
+
+void LinkedList::InsertTail(int data){
+    //go from head to the current tail, 
+    //then attach new node to the tail which becomes new tails
+    Node *newNode = new Node();
+    newNode->data = data;
+    newNode->next = NULL;
+
+    Node *curNode = this->head;
+    //go to tail
+    while(curNode->next != NULL){
+        curNode = curNode->next->next;
+    }
+    //at the tail add the new node in
+    curNode->next = newNode;
+
+    cout << "The new node added at the Tail of the list. List size is: " << ++this->nCount<<"\n";
+}
+
+void LinkedList::RemoveTail(){
+    Node *pDel;
+
+    Node *curNode = this->head;
+    //go to the node right before tail
+    while(curNode->next->next != NULL){
+        curNode = curNode->next->next;
+    }
+    
+    pDel = curNode->next;
+    curNode->next = NULL;
+    delete pDel;
+    cout << "The tail node already removed. The size of list remand is: " << --this->nCount<<"\n";
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 
+#include "LinkedList.h"
 using namespace std;
 
 
 int main() {
-    cout << "Hello Easy C++ project!" << std::endl;
-    cout << "Something in the next line \n";
+    
+    LinkedList *lSample = new LinkedList();
+    cout << "Samle List init, size is: " << lSample->Size() <<"\n";
+
+    //insert head
+    lSample->InsertHead(1);
+    lSample->InsertHead(2);
+    lSample->InsertHead(3);
+    lSample->InsertTail(4);
+    cout <<"size of the list by Size method "<< lSample->Size() << "\n";
+    lSample->Traverse();
+
+    return 1;
 }
